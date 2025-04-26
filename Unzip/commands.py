3721d14178d8405ca9e3f5e69770d3607e7f7eeb
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-04-26 06:55:25 +0530

    Update commands.py

--- Unzip/commands.py
+++ Unzip/commands.py
@@ -15,11 +15,11 @@ async def start(client, message):
     reply_markup = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton("📍 Update Channel", url="https://t.me/NT_BOT_CHANNEL"),
+            InlineKeyboardButton("📍 Update Channel", url="https://t.me/myserver23"),
         ],
         [
-            InlineKeyboardButton("👥 Support Group", url="https://t.me/NT_BOTS_SUPPORT"),
-            InlineKeyboardButton("👩‍💻 Developer", url="https://t.me/LISA_FAN_LK"),
+            InlineKeyboardButton("👥 Support Group", url="https://t.me/myservergroup23"),
+            InlineKeyboardButton("👩‍💻 Developer", url="https://t.me/myserver23"),
         ] 
    ]
   )
@@ -43,7 +43,7 @@ async def help_command(client, message):
         "/start - Start the bot and get the welcome message\n"
         "/help - Get help on how to use the bot\n\n"
         "To unzip a file, simply send me a ZIP file and I will extract its contents and send them back to you.\n\n"
-        "©️ Channel : @NT_BOT_CHANNEL"
+        "©️ Channel : @MYSERVER23"
     )
     await message.reply(help_message)
 
